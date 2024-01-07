c99b7d32f2cb4fe734c39bb985d88cbeaf22d0a2
Gonçalo Basto <goncalo.basto@gbit.pt>
2024-01-07 19:46:57 +0000

    cookieOptions

--- src/actions/authorization/interactions.js
+++ src/actions/authorization/interactions.js
@@ -1,11 +1,11 @@
-import * as url from 'node:url';
+import * as url from "node:url";
 
-import upperFirst from '../../helpers/_/upper_first.js';
-import camelCase from '../../helpers/_/camel_case.js';
-import * as ssHandler from '../../helpers/samesite_handler.js';
-import * as errors from '../../helpers/errors.js';
-import instance from '../../helpers/weak_cache.js';
-import nanoid from '../../helpers/nanoid.js';
+import upperFirst from "../../helpers/_/upper_first.js";
+import camelCase from "../../helpers/_/camel_case.js";
+import * as ssHandler from "../../helpers/samesite_handler.js";
+import * as errors from "../../helpers/errors.js";
+import instance from "../../helpers/weak_cache.js";
+import nanoid from "../../helpers/nanoid.js";
 
 /* eslint-disable no-await-in-loop */
 
@@ -14,20 +14,22 @@ export default async function interactions(resumeRouteName, ctx, next) {
   let failedCheck;
   let prompt;
 
-  const { policy, url: interactionUrl } = instance(oidc.provider).configuration('interactions');
+  const { policy, url: interactionUrl } = instance(oidc.provider).configuration("interactions");
 
   for (const { name, checks, details: promptDetails } of policy) {
-    let results = (await Promise.all([...checks].map(async ({
-      reason, description, error, details, check,
-    }) => {
-      if (await check(ctx)) {
-        return {
-          [reason]: { error, description, details: await details(ctx) },
-        };
-      }
+    let results = (
+      await Promise.all(
+        [...checks].map(async ({ reason, description, error, details, check }) => {
+          if (await check(ctx)) {
+            return {
+              [reason]: { error, description, details: await details(ctx) },
+            };
+          }
 
-      return undefined;
-    }))).filter(Boolean);
+          return undefined;
+        })
+      )
+    ).filter(Boolean);
 
     if (results.length) {
       results = Object.assign({}, ...results);
@@ -37,14 +39,14 @@ export default async function interactions(resumeRouteName, ctx, next) {
         details: Object.assign(
           {},
           await promptDetails(ctx),
-          ...Object.values(results).map((r) => r.details),
+          ...Object.values(results).map((r) => r.details)
         ),
       };
 
       const [[, { error, description }]] = Object.entries(results);
       failedCheck = {
-        error: error || 'interaction_required',
-        error_description: description || 'interaction is required from the end-user',
+        error: error || "interaction_required",
+        error_description: description || "interaction is required from the end-user",
       };
       break;
     }
@@ -54,7 +56,10 @@ export default async function interactions(resumeRouteName, ctx, next) {
   if (!prompt) {
     // check there's an accountId to continue
     if (!oidc.session.accountId) {
-      throw new errors.AccessDenied(undefined, 'authorization request resolved without requesting interactions but no account id was resolved');
+      throw new errors.AccessDenied(
+        undefined,
+        "authorization request resolved without requesting interactions but no account id was resolved"
+      );
     }
 
     // check there's something granted to continue
@@ -62,23 +67,25 @@ export default async function interactions(resumeRouteName, ctx, next) {
     // when no scope parameter was provided and none is injected by the AS policy access is
     // denied rather then issuing a code/token without scopes
     if (
-      !oidc.grant.getOIDCScopeFiltered(oidc.requestParamOIDCScopes)
-      && Object.keys(ctx.oidc.resourceServers)
-        .every(
-          (resource) => !oidc.grant.getResourceScopeFiltered(resource, oidc.requestParamScopes),
-        )
+      !oidc.grant.getOIDCScopeFiltered(oidc.requestParamOIDCScopes) &&
+      Object.keys(ctx.oidc.resourceServers).every(
+        (resource) => !oidc.grant.getResourceScopeFiltered(resource, oidc.requestParamScopes)
+      )
     ) {
-      throw new errors.AccessDenied(undefined, 'authorization request resolved without requesting interactions but no scope was granted');
+      throw new errors.AccessDenied(
+        undefined,
+        "authorization request resolved without requesting interactions but no scope was granted"
+      );
     }
 
-    oidc.provider.emit('authorization.accepted', ctx);
+    oidc.provider.emit("authorization.accepted", ctx);
     await next();
     return;
   }
 
   // if interaction needed but prompt=none => throw;
   try {
-    if (oidc.promptPending('none')) {
+    if (oidc.promptPending("none")) {
       const className = upperFirst(camelCase(failedCheck.error));
       if (errors[className]) {
         throw new errors[className](failedCheck.error_description);
@@ -95,7 +102,7 @@ export default async function interactions(resumeRouteName, ctx, next) {
 
   const uid = nanoid();
 
-  const cookieOptions = instance(oidc.provider).configuration('cookies.short');
+  const cookieOptions = instance(oidc.provider).configuration("cookies.short");
   const returnTo = oidc.urlFor(resumeRouteName, {
     uid,
   });
@@ -113,42 +120,32 @@ export default async function interactions(resumeRouteName, ctx, next) {
     ...(oidc.deviceCode ? { deviceCode: oidc.deviceCode.jti } : undefined),
   });
 
-  let ttl = instance(ctx.oidc.provider).configuration('ttl.Interaction');
+  let ttl = instance(ctx.oidc.provider).configuration("ttl.Interaction");
 
-  if (typeof ttl === 'function') {
+  if (typeof ttl === "function") {
     ttl = ttl(ctx, interactionSession);
   }
 
   await interactionSession.save(ttl);
-  ctx.oidc.entity('Interaction', interactionSession);
+  ctx.oidc.entity("Interaction", interactionSession);
 
   const destination = await interactionUrl(ctx, interactionSession);
 
-  ssHandler.set(
-    ctx.oidc.cookies,
-    oidc.provider.cookieName('interaction'),
-    uid,
-    {
-      path: url.parse(destination).pathname,
-      ...cookieOptions,
-      maxAge: ttl * 1000,
-    },
-  );
+  ssHandler.set(ctx.oidc.cookies, oidc.provider.cookieName("interaction"), uid, {
+    path: url.parse(destination).pathname,
+    ...cookieOptions,
+    maxAge: ttl * 1000,
+  });
 
-  ssHandler.set(
-    ctx.oidc.cookies,
-    oidc.provider.cookieName('resume'),
-    uid,
-    {
-      ...cookieOptions,
-      path: url.parse(returnTo).pathname,
-      domain: undefined,
-      httpOnly: true,
-      maxAge: ttl * 1000,
-    },
-  );
+  ssHandler.set(ctx.oidc.cookies, oidc.provider.cookieName("resume"), uid, {
+    path: url.parse(returnTo).pathname,
+    ...cookieOptions,
+    domain: undefined,
+    httpOnly: true,
+    maxAge: ttl * 1000,
+  });
 
-  oidc.provider.emit('interaction.started', ctx, prompt);
+  oidc.provider.emit("interaction.started", ctx, prompt);
   ctx.status = 303;
   ctx.redirect(destination);
 }
