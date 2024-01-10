a6449f46fb713727cc2253368f2e15c4de63f65c
Gonçalo Basto <goncalo.basto@gbit.pt>
2024-01-10 21:02:50 +0000

    No prompt

--- src/actions/end_session.js
+++ src/actions/end_session.js
@@ -1,27 +1,39 @@
-import * as crypto from 'node:crypto';
-import * as util from 'node:util';
+import * as crypto from "node:crypto";
+import * as util from "node:util";
 
-import { InvalidClient, InvalidRequest, OIDCProviderError } from '../helpers/errors.js';
-import * as JWT from '../helpers/jwt.js';
-import redirectUri from '../helpers/redirect_uri.js';
-import instance from '../helpers/weak_cache.js';
-import rejectDupes from '../shared/reject_dupes.js';
-import bodyParser from '../shared/conditional_body.js';
-import paramsMiddleware from '../shared/assemble_params.js';
-import sessionMiddleware from '../shared/session.js';
-import revoke from '../helpers/revoke.js';
-import noCache from '../shared/no_cache.js';
-import * as ssHandler from '../helpers/samesite_handler.js';
-import formPost from '../response_modes/form_post.js';
+import { InvalidClient, InvalidRequest, OIDCProviderError } from "../helpers/errors.js";
+import * as JWT from "../helpers/jwt.js";
+import redirectUri from "../helpers/redirect_uri.js";
+import instance from "../helpers/weak_cache.js";
+import rejectDupes from "../shared/reject_dupes.js";
+import bodyParser from "../shared/conditional_body.js";
+import paramsMiddleware from "../shared/assemble_params.js";
+import sessionMiddleware from "../shared/session.js";
+import revoke from "../helpers/revoke.js";
+import noCache from "../shared/no_cache.js";
+import * as ssHandler from "../helpers/samesite_handler.js";
+import formPost from "../response_modes/form_post.js";
 
-const parseBody = bodyParser.bind(undefined, 'application/x-www-form-urlencoded');
+const parseBody = bodyParser.bind(undefined, "application/x-www-form-urlencoded");
 const randomFill = util.promisify(crypto.randomFill);
 
 export const init = [
   noCache,
   sessionMiddleware,
   parseBody,
-  paramsMiddleware.bind(undefined, new Set(['id_token_hint', 'post_logout_redirect_uri', 'state', 'ui_locales', 'client_id', 'logout_hint'])),
+  paramsMiddleware.bind(
+    undefined,
+    new Set([
+      "id_token_hint",
+      "post_logout_redirect_uri",
+      "state",
+      "ui_locales",
+      "client_id",
+      "logout_hint",
+      "xsrf",
+      "logout",
+    ])
+  ),
   rejectDupes.bind(undefined, {}),
 
   async function endSessionChecks(ctx, next) {
@@ -31,18 +43,20 @@ export const init = [
     if (params.id_token_hint) {
       try {
         const idTokenHint = JWT.decode(params.id_token_hint);
-        ctx.oidc.entity('IdTokenHint', idTokenHint);
+        ctx.oidc.entity("IdTokenHint", idTokenHint);
       } catch (err) {
-        throw new InvalidRequest('could not decode id_token_hint', undefined, err.message);
+        throw new InvalidRequest("could not decode id_token_hint", undefined, err.message);
       }
-      const { payload: { aud: clientId } } = ctx.oidc.entities.IdTokenHint;
+      const {
+        payload: { aud: clientId },
+      } = ctx.oidc.entities.IdTokenHint;
 
       if (params.client_id && params.client_id !== clientId) {
-        throw new InvalidRequest('client_id does not match the provided id_token_hint');
+        throw new InvalidRequest("client_id does not match the provided id_token_hint");
       }
       client = await ctx.oidc.provider.Client.find(clientId);
       if (!client) {
-        throw new InvalidClient('unrecognized id_token_hint audience', 'client not found');
+        throw new InvalidClient("unrecognized id_token_hint audience", "client not found");
       }
       try {
         await ctx.oidc.provider.IdToken.validate(params.id_token_hint, client);
@@ -51,20 +65,20 @@ export const init = [
           throw err;
         }
 
-        throw new InvalidRequest('could not validate id_token_hint', undefined, err.message);
+        throw new InvalidRequest("could not validate id_token_hint", undefined, err.message);
       }
-      ctx.oidc.entity('Client', client);
+      ctx.oidc.entity("Client", client);
     } else if (params.client_id) {
       client = await ctx.oidc.provider.Client.find(params.client_id);
       if (!client) {
-        throw new InvalidClient('client is invalid', 'client not found');
+        throw new InvalidClient("client is invalid", "client not found");
       }
-      ctx.oidc.entity('Client', client);
+      ctx.oidc.entity("Client", client);
     }
 
     if (client && params.post_logout_redirect_uri !== undefined) {
       if (!client.postLogoutRedirectUriAllowed(params.post_logout_redirect_uri)) {
-        throw new InvalidRequest('post_logout_redirect_uri not registered');
+        throw new InvalidRequest("post_logout_redirect_uri not registered");
       }
     } else if (params.post_logout_redirect_uri !== undefined) {
       params.post_logout_redirect_uri = undefined;
@@ -77,7 +91,7 @@ export const init = [
     // TODO: generic xsrf middleware to remove this
     let secret = Buffer.allocUnsafe(24);
     await randomFill(secret);
-    secret = secret.toString('hex');
+    secret = secret.toString("hex");
 
     ctx.oidc.session.state = {
       secret,
@@ -85,45 +99,22 @@ export const init = [
       state: ctx.oidc.params.state,
       postLogoutRedirectUri: ctx.oidc.params.post_logout_redirect_uri,
     };
-
-    const action = ctx.oidc.urlFor('end_session_confirm');
-
-    if (ctx.oidc.session.accountId) {
-      ctx.type = 'html';
-      ctx.status = 200;
-
-      const formHtml = `<form id="op.logoutForm" method="post" action="${action}"><input type="hidden" name="xsrf" value="${secret}"/></form>`;
-      await instance(ctx.oidc.provider).configuration('features.rpInitiatedLogout.logoutSource')(ctx, formHtml);
-    } else {
-      formPost(ctx, action, {
-        xsrf: secret,
-        logout: 'yes',
-      });
-    }
+    ctx.oidc.params.logout = true;
 
     await next();
   },
-];
-
-export const confirm = [
-  noCache,
-  sessionMiddleware,
-  parseBody,
-  paramsMiddleware.bind(undefined, new Set(['xsrf', 'logout'])),
-  rejectDupes.bind(undefined, {}),
 
   async function checkLogoutToken(ctx, next) {
     if (!ctx.oidc.session.state) {
-      throw new InvalidRequest('could not find logout details');
-    }
-    if (ctx.oidc.session.state.secret !== ctx.oidc.params.xsrf) {
-      throw new InvalidRequest('xsrf token invalid');
+      throw new InvalidRequest("could not find logout details");
     }
     await next();
   },
 
   async function endSession(ctx, next) {
-    const { oidc: { session, params } } = ctx;
+    const {
+      oidc: { session, params },
+    } = ctx;
     const { state } = session;
 
     const {
@@ -143,12 +134,16 @@ export const confirm = [
             const sid = session.sidFor(client.clientId);
             if (client.backchannelLogoutUri) {
               const { accountId } = session;
-              back.push(client.backchannelLogout(accountId, sid)
-                .then(() => {
-                  ctx.oidc.provider.emit('backchannel.success', ctx, client, accountId, sid);
-                }, (err) => {
-                  ctx.oidc.provider.emit('backchannel.error', ctx, err, client, accountId, sid);
-                }));
+              back.push(
+                client.backchannelLogout(accountId, sid).then(
+                  () => {
+                    ctx.oidc.provider.emit("backchannel.success", ctx, client, accountId, sid);
+                  },
+                  (err) => {
+                    ctx.oidc.provider.emit("backchannel.error", ctx, err, client, accountId, sid);
+                  }
+                )
+              );
             }
           }
         }
@@ -158,7 +153,7 @@ export const confirm = [
     }
 
     if (state.clientId) {
-      ctx.oidc.entity('Client', await ctx.oidc.provider.Client.find(state.clientId));
+      ctx.oidc.entity("Client", await ctx.oidc.provider.Client.find(state.clientId));
     }
 
     if (params.logout) {
@@ -171,23 +166,18 @@ export const confirm = [
             if (grantId && !session.authorizationFor(clientId).persistsLogout) {
               await revoke(ctx, grantId);
             }
-          }),
+          })
         );
       }
 
       await session.destroy();
 
-      ssHandler.set(
-        ctx.oidc.cookies,
-        ctx.oidc.provider.cookieName('session'),
-        null,
-        opts,
-      );
+      ssHandler.set(ctx.oidc.cookies, ctx.oidc.provider.cookieName("session"), null, opts);
     } else if (state.clientId) {
       const grantId = session.grantIdFor(state.clientId);
       if (grantId && !session.authorizationFor(state.clientId).persistsLogout) {
         await revoke(ctx, grantId);
-        ctx.oidc.provider.emit('grant.revoked', ctx, grantId);
+        ctx.oidc.provider.emit("grant.revoked", ctx, grantId);
       }
       session.state = undefined;
       if (session.authorizations) {
@@ -199,15 +189,14 @@ export const confirm = [
     const usePostLogoutUri = state.postLogoutRedirectUri;
     const forwardClientId = !usePostLogoutUri && !params.logout && state.clientId;
     const uri = redirectUri(
-      usePostLogoutUri ? state.postLogoutRedirectUri : ctx.oidc.urlFor('end_session_success'),
+      usePostLogoutUri ? state.postLogoutRedirectUri : ctx.oidc.urlFor("end_session_success"),
       {
-        ...(usePostLogoutUri && state.state != null
-          ? { state: state.state } : undefined), // != intended
+        ...(usePostLogoutUri && state.state != null ? { state: state.state } : undefined), // != intended
         ...(forwardClientId ? { client_id: state.clientId } : undefined),
-      },
+      }
     );
 
-    ctx.oidc.provider.emit('end_session.success', ctx);
+    ctx.oidc.provider.emit("end_session.success", ctx);
 
     ctx.status = 303;
     ctx.redirect(uri);
@@ -216,17 +205,21 @@ export const confirm = [
   },
 ];
 
+export const confirm = [];
+
 export const success = [
   noCache,
-  paramsMiddleware.bind(undefined, new Set(['client_id'])),
+  paramsMiddleware.bind(undefined, new Set(["client_id"])),
   async function postLogoutSuccess(ctx) {
     if (ctx.oidc.params.client_id) {
       const client = await ctx.oidc.provider.Client.find(ctx.oidc.params.client_id);
       if (!client) {
-        throw new InvalidClient('client is invalid', 'client not found');
+        throw new InvalidClient("client is invalid", "client not found");
       }
-      ctx.oidc.entity('Client', client);
+      ctx.oidc.entity("Client", client);
     }
-    await instance(ctx.oidc.provider).configuration('features.rpInitiatedLogout.postLogoutSuccessSource')(ctx);
+    await instance(ctx.oidc.provider).configuration(
+      "features.rpInitiatedLogout.postLogoutSuccessSource"
+    )(ctx);
   },
 ];
